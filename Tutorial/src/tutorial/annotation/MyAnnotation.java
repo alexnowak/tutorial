1e1f1e6c384c3af3b82a226e567581624bc37c1d
alexnowak <alex.nowak@gmx.de>
2013-09-24 14:38:30 -0700

    minor changes

--- Tutorial/src/tutorial/annotation/MyAnnotation.java
+++ Tutorial/src/tutorial/annotation/MyAnnotation.java
@@ -18,7 +18,7 @@ import java.lang.annotation.Documented;
 
 @ClassPreamble (
    author = "John Doe",
-   date = "3/17/2002",
+   date = "3/17/2013",
    currentRevision = 6,
    lastModified = "4/12/2004",
    lastModifiedBy = "Jane Doe",
